471affbc533b327a1e45c6878956ca5bccde5904
WillVT <williamvantrump@gmail.com>
2015-05-19 10:08:38 -0700

    Modification of Assignment Template
    Completion of R programming course assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  mat_inv <- NULL
+  set <- function(y){
+    mat <<- y
+    mat_inv <<- NULL
+  }
+  get <- function() mat
+  set_inv <- function(inv_mat) mat_inv <<- inv_mat
+  get_inv <- function() mat_inv
+  list(set = set, get = get, get_inv = get_inv, set_inv = set_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m_cache <- x$get_inv()
+  if(!is.null(m_cache)){
+    message("getting cached inverted matrix")
+    return(m_cache)
+  }
+  data <- x$get()
+  mat_out <- solve(data, ...)
+  x$set_inv(mat_out)
+  mat_out
 }
